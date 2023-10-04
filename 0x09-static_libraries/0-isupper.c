3124ccc946a92ebf91a7ffa5d5858aeba3d9064d
gabafo1 <gabrielafolabi@gmail.com>
2023-10-04 12:15:53 -0700

    commit

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -3,12 +3,11 @@
  * _isupper - uppercase letters
  * @c: char to check
  *
- * Return: 0 or 1
+ * Return: 1 or 0
  */
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-	return (0);
+	return (c >= 'A' && c <= 'Z');
 }
+
