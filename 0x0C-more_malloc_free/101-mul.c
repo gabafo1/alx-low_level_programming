674100440f1e65c7f7cdcb89a83d5b1bc45a3dd9
gabafo1 <gabrielafolabi@gmail.com>
2023-10-06 20:18:02 -0700

    commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -34,19 +34,26 @@ int _strlen(char *s)
 	}
 	return (i);
 }
+/**
+ * errors - handles errors for main
+ */
 void errors(void)
 {
 	printf("Error\n");
 	exit(98);
 }
-
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
@@ -78,9 +85,6 @@ int main(int argc, char *argv[])
 		if (a)
 			_putchar(result[i] + '0');
 	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
 	free(result);
-	return 0;
+	return (0);
 }
