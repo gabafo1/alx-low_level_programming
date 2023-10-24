f90acebef3f8da4a9af003252340e3446feef132
gabrielafolabi@gmail.com <gabrieafolabi@gmail.com>
2023-10-24 16:41:58 -0700

    commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,36 +11,34 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *new;
-	listint_t *temp = *head;
-
-	new = malloc(sizeof(listint_t));
-	if (!new || !head)
+	listint_t *node, *new_node = malloc(sizeof(listint_t));
+	unsigned int i = 0;
+	
+	if (!head || !new_node)
 		return (NULL);
 
-	new->n = n;
-	new->next = NULL;
+	new_node->n = n;
+	new_node->next = NULL;
 
-	if (idx == 0)
+	if (!idx)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
-
-
-		for (i = 0; temp && i < idx; i++)
-		{
-			if (i == idx - 1)
-			{
-				new->next = temp->next;
-				temp->next = new;
-				return (new);
-			}
-			else
-				temp = temp->next;
-		}
-
-	return (NULL);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
+
+	node = *head;
+	while (node)
+	{
+		if (i == idx - 1)
+		{
+			new_node->next = node->next;
+			node->next = new_node;
+			return (new_node);
+		}
+		i++
+		node = node->next;
+	}
+	free(new_node)
+	return (NULL);
 }
