35a71a498bb48ba5794a7aa5e3eb6d87609147f7
gabafo1 <gabrieafolabi@gmail.com>
2023-10-24 16:09:08 -0700

    commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,15 +31,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 		for (i = 0; temp && i < idx; i++)
 		{
-		if (i == idx - 1)
-		{
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
+			if (i == idx - 1)
+			{
+				new->next = temp->next;
+				temp->next = new;
+				return (new);
+			}
+			else
+				temp = temp->next;
 		}
-		else
-			temp = temp->next;
-	}
 
 	return (NULL);
+	}
 }
